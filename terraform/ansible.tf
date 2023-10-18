9e83fc517fd674ef9cf8e71ced9fe2af06962b9c
Mike Sedillo <msedilloanthony@lusv7h022jwgn.teslamotors.com>
2023-10-18 07:51:56 -0700

    Reconfiguring ansible structure

---
+++ terraform/ansible.tf
@@ -0,0 +1,16 @@
+provider "local" {
+  // Configuration options
+}
+
+resource "local_file" "AnsibleInventory" {
+  content = <<-EOF
+    [varnish]
+    ${aws_instance.varnish.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=./ansible_private_key.pem
+
+    [client]
+    ${aws_instance.client.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=./ansible_private_key.pem
+  EOF
+
+  filename = "${path.module}/../ansible/inventory.ini"
+}
+
